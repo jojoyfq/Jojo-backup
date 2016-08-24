e22d979a5e9ab7514e7a8657ed94e2ae43957718
apple <apple@172.25.103.108>
2016-08-24 18:10:33 +0800

    First commit

---
+++ MerlionBank-ejb/src/java/ejb/session/stateless/RecordSessionBean.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author apple
+ */
+@Stateless
+public class RecordSessionBean implements RecordSessionBeanLocal {
+    @PersistenceContext
+    private EntityManager em;
+    
+}
