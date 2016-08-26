c9fcd074349706e7bc3604e4ea8cc825050b26c2
jojoyfq <yeahfq@gmail.com>
2016-08-26 10:13:43 +0800

    commonsession

---
+++ MerlionBank-ejb/src/java/CommonEntity/Session/AccountManagementSessionBean.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CommonEntity.Session;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author a0113893
+ */
+@Stateless
+public class AccountManagementSessionBean implements AccountManagementSessionBeanLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
