57e37e0a9c519ee11f62a57e89d479fe99c434d5
jojoyfq <yeahfq@gmail.com>
2016-09-16 20:57:44 +0800

    simulation system
    finish two trransfer

--- MerlionBank-ejb/src/java/Simulation/Session/ExternalTransferSessionBean.java
+++ MerlionBank-ejb/src/java/Simulation/Session/ExternalTransferSessionBean.java
@@ -5,7 +5,17 @@
  */
 package Simulation.Session;
 
+import CommonEntity.Staff;
+import DepositEntity.FixedDepositAccount;
+import DepositEntity.SavingAccount;
+import Exception.AccountNotExistedException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -13,7 +23,42 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class ExternalTransferSessionBean implements ExternalTransferSessionBeanLocal {
+@PersistenceContext
+    private EntityManager em;
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+@Override
+public boolean transferSavingAccount(Long accountNumber, BigDecimal amount)throws AccountNotExistedException{
+   Query q = em.createQuery("SELECT a FROM SavingAccount a WHERE a.accountNumber = :accountNumber");
+        q.setParameter("accountNumber", accountNumber);
+        List<SavingAccount> savingAccounts = new ArrayList(q.getResultList()); 
+        if (savingAccounts.isEmpty()) {
+            throw new AccountNotExistedException ("Account is invalid");
+        }     
+            SavingAccount savingAccount=savingAccounts.get(0);
+        BigDecimal targetBalance=savingAccount.getBalance().add(amount);
+        BigDecimal targetAvailableBalance=savingAccount.getAvailableBalance().add(amount);
+        savingAccount.setBalance(targetBalance);
+        savingAccount.setAvailableBalance(targetAvailableBalance);
+        em.persist(savingAccount);
+        em.flush();
+        return true;
+}
+
+@Override
+public boolean transferFixedDepositAccount(Long accountNumber, BigDecimal amount)throws AccountNotExistedException{
+   Query q = em.createQuery("SELECT a FROM FixedDepositAccount a WHERE a.accountNumber = :accountNumber");
+        q.setParameter("accountNumber", accountNumber);
+        List<FixedDepositAccount> fixedDepositAccounts = new ArrayList(q.getResultList()); 
+        if (fixedDepositAccounts.isEmpty()) {
+            throw new AccountNotExistedException ("Account is invalid");
+        }     
+            FixedDepositAccount fixedDepositAccount=fixedDepositAccounts.get(0);
+        BigDecimal targetBalance=fixedDepositAccount.getBalance().add(amount);
+        fixedDepositAccount.setBalance(targetBalance);
+        em.persist(fixedDepositAccount);
+        em.flush();
+        return true;
+}
+
+
 }
