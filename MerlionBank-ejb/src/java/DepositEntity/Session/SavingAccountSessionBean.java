66296609b2263e63acc7e627c46f3f7b3e18fdd3
RuijiaWang <ruijia1993@gmail.com>
2016-09-18 16:48:29 +0800

    lala

--- MerlionBank-ejb/src/java/DepositEntity/Session/SavingAccountSessionBean.java
+++ MerlionBank-ejb/src/java/DepositEntity/Session/SavingAccountSessionBean.java
@@ -91,6 +91,7 @@ public class SavingAccountSessionBean implements SavingAccountSessionBeanLocal {
                 }
             }
             return savingAccountNumbers;
+            //haha
         }
     }
 
