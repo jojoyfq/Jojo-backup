274fc31838419d34e9b5e0a4b48e2679127220f4
RuijiaWang <ruijia1993@gmail.com>
2016-09-24 14:28:21 +0800

    update from master

--- MerlionBank-ejb/src/java/CommonEntity/Session/StaffManagementSessionBeanLocal.java
+++ MerlionBank-ejb/src/java/CommonEntity/Session/StaffManagementSessionBeanLocal.java
@@ -100,8 +100,6 @@ public Long updateStaffInfo(Long adminId, Long staffId, String staffIc, String s
     public Long lockAccount(Long staffId);
     
     public StaffRole getRoleByRoleName(String roleName);
-
-    public StaffRole viewRole(Long roleId);
     
     public void recordStaffAction(Long staffId, String actionDescription, Long customerId);
 }
