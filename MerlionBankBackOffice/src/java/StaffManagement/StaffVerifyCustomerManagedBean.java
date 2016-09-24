02133306b82bec565439e7fb1218a6cde07e6c47
jojoyfq <yeahfq@gmail.com>
2016-09-24 10:13:34 +0800

    SIT
    SIT update3

--- MerlionBankBackOffice/src/java/StaffManagement/StaffVerifyCustomerManagedBean.java
+++ MerlionBankBackOffice/src/java/StaffManagement/StaffVerifyCustomerManagedBean.java
@@ -108,6 +108,7 @@ public class StaffVerifyCustomerManagedBean implements Serializable {
                 RequestContext.getCurrentInstance().showMessageInDialog(sysMessage);
 
             } else if (!customer.getFixedDepositeAccounts().isEmpty()) {
+                System.out.println("*******go in to fixed account verification");
                  if(result.equals("true")){
                     result = "approve";
                 }else{
