5fbd2269de7683ef9abe44992efda589a406c26c
RuijiaWang <ruijia1993@gmail.com>
2016-09-24 15:09:29 +0800

    add back prof code

--- MerlionBank-ejb/src/java/CommonEntity/Session/StaffManagementSessionBeanLocal.java
+++ MerlionBank-ejb/src/java/CommonEntity/Session/StaffManagementSessionBeanLocal.java
@@ -102,4 +102,6 @@ public Long updateStaffInfo(Long adminId, Long staffId, String staffIc, String s
     public StaffRole getRoleByRoleName(String roleName);
     
     public void recordStaffAction(Long staffId, String actionDescription, Long customerId);
+
+    public StaffRole viewRole(Long roleId);
 }
